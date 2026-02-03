31caba7867abd69c6b6cc985975cd7714f4d5b33
Khoi2746 <phamkhoi2746@gmail.com>
2026-02-03 22:51:52 +0700

    KhoiPham
    UpdateFull

--- PhanMemQuanLyThuVien/src/main/java/com/poly/DaoImpl/BorrowDAOImpl.java
+++ PhanMemQuanLyThuVien/src/main/java/com/poly/DaoImpl/BorrowDAOImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 /**
  *
- * @author LENOVO
+ * @author 
  */
 public class BorrowDAOImpl implements BorrowDAO{
     
--- PhanMemQuanLyThuVien/src/test/java/com/fpoly/test/AllTestsSuite.java
+++ PhanMemQuanLyThuVien/src/test/java/com/fpoly/test/AllTestsSuite.java
@@ -6,7 +6,7 @@ package com.fpoly.test;
 
 /**
  *
- * @author LENOVO
+ * @author 
  */
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
